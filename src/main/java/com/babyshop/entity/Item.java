e2fbbfecd6e0e23b6abfdf5168f7deab3f08b343
Yasith Ishara Perera <yasithishara@gmail.com>
2024-12-29 21:14:45 +0530

    check discount

--- src/main/java/com/babyshop/entity/Item.java
+++ src/main/java/com/babyshop/entity/Item.java
@@ -5,16 +5,26 @@ public class Item {
     private String itemName;
     private double unitPrice;
     private double quantity;
+    private double discount; // to add a discount
     private double total;
 
     public Item() {
     }
 
-    public Item(String itemName, double unitPrice, double quantity, double total) {
+    public Item(String itemName, double unitPrice, double quantity, double discount, double total) {
         this.itemName = itemName;
         this.unitPrice = unitPrice;
         this.quantity = quantity;
+        this.discount = discount;
         this.total = total;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
     }
 
     public String getItemName() {
@@ -51,9 +61,11 @@ public class Item {
 
     @Override
     public String toString() {
-        return "Item{" + "itemName=" + itemName + 
-                ", unitPrice=" + unitPrice + 
-                ", quantity=" + quantity + 
-                ", total=" + total + '}';
+        return "Item{" + "itemName=" + itemName +
+                ", unitPrice=" + unitPrice +
+                ", quantity=" + quantity +
+                ", discount=" + discount +
+                ", total=" + total +
+               '}';
     }
 }
